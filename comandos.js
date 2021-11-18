8569a2759ddfb0e250a96fb8d4e78b230ad8a157
Pimegonho <alvarowrorp1234@hotmail.com>
2021-11-18 01:52:31 -0300

    Add files via upload

---
+++ comandos.js
@@ -0,0 +1,58 @@
+/*
+
+OPERADORES (+, -, *, /, etc...)
+    ++; incremento. Pode ser usado antes ou depois. Exemplo: n++ (primeiro n depois soma) ou ++n (primeiro soma depois n).
+    --; decremento. Pode ser usado antes ou depois. Exemplo: n-- (primeiro n depois subtrai) ou --n (primeiro subtrai depois n).
+    %; resto da divisão. Exemplo: 5 % 2 = 4 resto 1.
+    **; elevado a. Exemplo: 3 ** 4 = 81 (3 * 3 * 3 * 3).
+    ==; igualdade (valor).
+    !=; diferente (valor).
+    ===; igualdade (valor e tipo).
+    !==; diferente (valor e tipo).
+    &&; e.
+    ||; ou.
+
+var; declara uma variável.
+
+alert(); joga um alerta na tela.
+
+prompt(); recebe entrada de dados.
+
+console.log(); exibe conteúdo no console.
+
+typeof; retorna o tipo da variável.
+
+parseInt; retorna a var como número inteiro.
+parseFloat; retorna a var como número real.
+
+Math.random(); retorna um número aleatório entre 0 e 1, porém nunca 1.
+
+document.write(); printa o elemento na página. Evitar usar.
+document.querySelector(); retorna o primeiro elemento CSS especificado.
+document.querySelectorAll(); retorna todos os elementos CSS especificados.
+
+.innerHTML; insere ou remove conteúdo dentro do html.
+
+ESTRUTURAS
+    while(condição){
+
+    } //usado quando não se sabe o número de repetições.
+
+    do{
+
+    }while(condição)
+
+    switch(){
+        case numero ou "string":
+            código;
+            break;
+        case numero...
+        default:
+            código;
+    }
+
+    for(iniciar; condição; incremento){
+
+    } //usado quando se sabe o número de repetições.
+
+*/
